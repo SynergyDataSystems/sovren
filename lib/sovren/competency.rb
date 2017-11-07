f6008c8a474e160cba9ade8f73f3b2bc663dec03
Nic Reed <nreed@patriotsoftware.com>
2017-11-07 11:42:13 -0500

    Add competency id to parsed compentencies.

--- lib/sovren/competency.rb
+++ lib/sovren/competency.rb
@@ -1,12 +1,13 @@
 module Sovren
   class Competency
-    attr_accessor :name, :months, :last_used_date
+    attr_accessor :name, :competency_id, :months, :last_used_date
 
     def self.parse(competencies)
       return Array.new if competencies.nil?
       results = competencies.css('Competency').collect do |item|
         c = Competency.new
         c.name = item['name']
+        c.competency_id = item.css('CompetencyId').first['id'] rescue nil
         c.months = item.css('CompetencyEvidence NumericValue').text.to_i rescue nil
         c.last_used_date = Date.parse(item.css('CompetencyEvidence').first['lastUsed']) rescue nil
         c
